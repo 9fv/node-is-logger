6887d74591def2fe0e0cac44b912b8f418dc49cd
François Michaud <fmichaud@9fevrier.com>
2018-01-26 19:51:15 +0100

    Improve source and code coverage

--- test/is-logger.test.js
+++ test/is-logger.test.js
@@ -5,7 +5,7 @@ const should = require('should'); // eslint-disable-line no-unused-vars
 const bunyan = require('bunyan');
 const winston = require('winston');
 
-const isALogger = require('../lib/is-logger');
+const isLogger = require('../lib/is-logger');
 
 
 /**
@@ -26,73 +26,97 @@ beforeEach(() => {
 /**
  * Base unit tests.
  */
-describe('Unit testing for [Function=isALogger]', () => {
+describe('Unit testing for [Function=isLogger]', () => {
   it('should be a function', () => {
-    (isALogger).should.be.a.Function();
+    (isLogger).should.be.a.Function();
   });
 
   it('should return a boolean', () => {
-    (isALogger(null)).should.be.a.Boolean();
+    (isLogger(null)).should.be.a.Boolean();
+  });
+
+  it('should return a boolean if (options.debug=true)', () => {
+    (isLogger(null, {debug: true})).should.be.a.Boolean();
   });
 
   it('should return false if none logger is provided', () => {
-    (isALogger(null)).should.be.a.Boolean().which.is.False();
+    (isLogger(null)).should.be.a.Boolean().which.is.False();
+  });
+
+  it('should return false if none logger is provided and (options.debug=true)', () => {
+    (isLogger(null, {debug: true})).should.be.a.Boolean().which.is.False();
   });
 
   it('should return true if a valid logger is provided', () => {
-    (isALogger(this.bunyanLogger)).should.be.a.Boolean().which.is.True();
+    (isLogger(this.bunyanLogger)).should.be.a.Boolean().which.is.True();
+  });
+
+  it('should return true if a valid logger is provided and (options.debug=true)', () => {
+    (isLogger(this.bunyanLogger, {debug: true})).should.be.a.Boolean().which.is.True();
   });
 
   it('should return false if an invalid logger is provided', () => {
-    (isALogger(this.invalidLogger)).should.be.a.Boolean().which.is.False();
+    (isLogger(this.invalidLogger)).should.be.a.Boolean().which.is.False();
+  });
+
+  it('should return false if an invalid logger is provided and (options.debug=true)', () => {
+    (isLogger(this.invalidLogger, {debug: true})).should.be.a.Boolean().which.is.False();
   });
 });
 
 /**
  * Unit tests for a Bunyan logger.
  */
-describe('Unit testing for [Function=isALogger] using a Bunyan logger', () => {
+describe('Unit testing for [Function=isLogger] using a Bunyan logger', () => {
   it('should return a boolean', () => {
-    (isALogger(this.bunyanLogger)).should.be.a.Boolean();
+    (isLogger(this.bunyanLogger)).should.be.a.Boolean();
   });
 
   it('should return true when a Bunyan logger is provided', () => {
-    (isALogger(this.bunyanLogger)).should.be.a.Boolean().which.is.True();
+    (isLogger(this.bunyanLogger)).should.be.a.Boolean().which.is.True();
   });
 
   it('should return false when the Bunyan module is provided', () => {
-    (isALogger(bunyan)).should.be.a.Boolean().which.is.False();
+    (isLogger(bunyan)).should.be.a.Boolean().which.is.False();
   });
 
   it('should throw if the Bunyan module is provided', () => {
     (() => {
-      isALogger(bunyan, {throwOnFalse: true})
-    }).should.throw('NotALoggerError: Provided (logger: ?} does not contain a (method: debug)');
+      isLogger(bunyan, {throwOnFalse: true});
+    }).should.throw('NotALoggerError: Provided logger does not contain a (method: debug)');
   });
 });
 
 /**
  * Unit tests for a Winston logger.
  */
-describe('Unit testing for [Function=isALogger] using a Winston@2.4.0 logger', () => {
+describe('Unit testing for [Function=isLogger] using a Winston@2.4.0 logger', () => {
   it('should return a boolean', () => {
-    (isALogger(this.winstonLogger)).should.be.a.Boolean();
+    (isLogger(this.winstonLogger)).should.be.a.Boolean();
   });
 
-  it('should return true when a Winston@2.4.0  logger is provided', () => {
-    (isALogger(this.winstonLogger)).should.be.a.Boolean().which.is.True();
+  it('should return true when a Winston@2.4.0 logger is provided', () => {
+    (isLogger(this.winstonLogger)).should.be.a.Boolean().which.is.True();
+  });
+
+  it('should return true when a Winston@2.4.0 logger is provided and (options.debug=true)', () => {
+    (isLogger(this.winstonLogger, {debug: true})).should.be.a.Boolean().which.is.True();
   });
 });
 
 /**
  * Unit tests for the default Winston logger.
  */
-describe('Unit testing for [Function=isALogger] using the default logger of Winston@2.4.0', () => {
+describe('Unit testing for [Function=isLogger] using the default logger of Winston@2.4.0', () => {
   it('should return a boolean', () => {
-    (isALogger(this.winstonDefaultLogger)).should.be.a.Boolean();
+    (isLogger(this.winstonDefaultLogger)).should.be.a.Boolean();
   });
 
   it('should return true when the default logger of Winston@2.4.0 is provided', () => {
-    (isALogger(this.winstonDefaultLogger)).should.be.a.Boolean().which.is.True();
+    (isLogger(this.winstonDefaultLogger)).should.be.a.Boolean().which.is.True();
+  });
+
+  it('should return true when the default logger of Winston@2.4.0 is provided (options.debug=true)', () => {
+    (isLogger(this.winstonDefaultLogger, {debug: true})).should.be.a.Boolean().which.is.True();
   });
 });
