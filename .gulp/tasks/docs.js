9a1e0c7347e31c312c305ab8e47b153aada2c18b
François Michaud <fmichaud@9fevrier.com>
2018-01-28 14:16:51 +0100

    Enhancement: fix CodeClimate bugs.

--- .gulp/tasks/docs.js
+++ .gulp/tasks/docs.js
@@ -1,16 +1,19 @@
 const gulp = require('gulp');
 const g = require('gulp-load-plugins')({lazy: true});
+const path = require('path');
+
+const SETTINGS = require('../settings.json');
 
 
 gulp.task('docs', () => {
-  return gulp.src('src/*.js')
+  return gulp.src(path.join(SETTINGS.sources.path, SETTINGS.sources.pattern))
     //.pipe(g.jsdocToMarkdown({ template: fs.readFileSync('./readme.hbs', 'utf8') }))
     .pipe(g.jsdocToMarkdown())
     .on('error', (err) => {
       g.util.log(g.util.colors.red('jsdoc2md failed'), err.message);
     })
-    .pipe(g.rename((path) => {
-      path.extname = '.md'; // eslint-disable-next-line no-param-reassign
+    .pipe(g.rename((p) => {
+      p.extname = '.md'; // eslint-disable-next-line no-param-reassign
     }))
     .pipe(gulp.dest('doc'));
 });
--- .gulp/tasks/lint.js
+++ .gulp/tasks/lint.js
@@ -1,25 +1,41 @@
 const gulp = require('gulp');
 const g = require('gulp-load-plugins')({lazy: true});
+const path = require('path');
 
-gulp.task('lint-source', () => gulp.src('lib/**').pipe(g.eslint())
-  .pipe(g.eslint.format())
-  .pipe(g.eslint.failOnError()));
+const SETTINGS = require('../settings.json');
 
-gulp.task('lint-test', () => gulp.src('test/**').pipe(g.eslint({
-  extends: 'airbnb',
-  plugins: [
-    'mocha',
-  ],
-  rules: {
-    'mocha/no-exclusive-tests': 'error',
-  },
-}))
-  .pipe(g.eslint.format())
-  .pipe(g.eslint.failOnError()));
+/**
+ * Lint source.
+ *
+ * @param p {string} - Path and pattern.
+ * @returns {void|*}
+ */
+function lint(p) {
+  return () => {
+    gulp.src(p).pipe(g.eslint())
+      .pipe(g.eslint.format())
+      .pipe(g.eslint.failOnError());
+  };
+}
 
-gulp.task('lint-gulp-tasks', () => gulp.src('.gulp/tasks/**').pipe(g.eslint())
-  .pipe(g.eslint.format())
-  .pipe(g.eslint.failOnError()));
+/**
+ * Gulp task to lint source.
+ */
+gulp.task('lint-source', lint(path.join(SETTINGS.sources.pattern, SETTINGS.sources.pattern)));
 
+
+/**
+ * Gulp task to lint test.
+ */
+gulp.task('lint-test', lint(path.join(SETTINGS.tests.pattern, SETTINGS.tests.pattern)));
+
+/**
+ * Gulp task to lint gulp tasks ;-)
+ */
+gulp.task('lint-gulp-tasks', lint(path.join(SETTINGS.gulp.tasks.pattern, SETTINGS.gulp.tasks.pattern)));
+
+/**
+ * Gulp task to lint all sources.
+ */
 gulp.task('lint', ['lint-source', 'lint-test', 'lint-gulp-tasks']);
 
